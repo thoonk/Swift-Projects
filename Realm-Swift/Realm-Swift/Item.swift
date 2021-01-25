2c7c441f7a7448857011a9a63b880f4133daff4c
thoonk <dkrmrm878@gmail.com>
2021-01-25 18:01:13 +0900

    Add .gitignore

---
+++ Realm-Swift/Realm-Swift/Item.swift
@@ -0,0 +1,8 @@
+//
+//  Item.swift
+//  Realm-Swift
+//
+//  Created by 김태훈 on 2020/11/14.
+//
+
+import Foundation
---
+++ Realm-Swift/Realm-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Realm-Swift
+//
+//  Created by 김태훈 on 2020/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
