24362971134058c1b8c6f3e0a8aaca24a4f1cf1a
thoonk <dkrmrm878@gmail.com>
2021-03-04 23:20:33 +0900

    [Add] HalfModalVCTest tested the halfModalView

---
+++ HalfModalVCTest/HalfModalVCTest/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  HalfModalVCTest
+//
+//  Created by 김태훈 on 2021/03/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func showHalfModalView() {
+        let slideVC = HalfModalView()
+        slideVC.modalPresentationStyle = .custom
+        slideVC.transitioningDelegate = self
+        self.present(slideVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func onButton(_ sender: Any) {
+        showHalfModalView()
+    }
+    
+}
+
+extension ViewController: UIViewControllerTransitioningDelegate {
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        PresentationController(presentedViewController: presented, presenting: presenting)
+    }
+}
