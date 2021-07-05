0429670c166c0b58da3acf9c208b5a9ee9723560
thoonk <dkrmrm878@gmail.com>
2021-07-05 23:18:38 +0900

    [Add] SharingInstagramTest init project

---
+++ SharingInstagramTest/SharingInstagramTest/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SharingInstagramTest
+//
+//  Created by 김태훈 on 2021/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
