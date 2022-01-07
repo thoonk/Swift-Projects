cf6fc2ab30248e98c5fdcb7d69bc3bafcf164e2d
thoonk <dkrmrm878@naver.com>
2022-01-07 15:25:04 +0900

    [Add] handle the PIP mode bug
    bug: Turn on PIP mode -> Enter background -> Enter foreground after entering mutltasking -> Player does not return to original position.

--- VideoPlayerTest/VideoPlayerTest/SceneDelegate.swift
+++ VideoPlayerTest/VideoPlayerTest/SceneDelegate.swift
@@ -12,7 +12,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
@@ -25,12 +24,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         AppDelegate.shared.window = window
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
+    func sceneDidDisconnect(_ scene: UIScene) {}
 
     func sceneDidBecomeActive(_ scene: UIScene) {
         // 무음 모드일 때 비디오 재생 사운드 켜기
@@ -41,20 +35,48 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
     }
 
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
+    func sceneWillResignActive(_ scene: UIScene) {}
 
     func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
+        presentingVC()
     }
 
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
+    func sceneDidEnterBackground(_ scene: UIScene) {}
 }
 
+private extension SceneDelegate {
+    func presentingVC() {
+        if #available(iOS 13.0, *) {
+            let sceneDelegate = UIApplication.shared.connectedScenes
+                .first!.delegate as! SceneDelegate
+            if let playerVC = topViewController(base: sceneDelegate.window!.rootViewController) as? PlayerVC {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    playerVC.pipController?.stopPictureInPicture()
+                }
+            }
+        } else {
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            
+            if let playerVC = topViewController(base: appDelegate.window!.rootViewController) as? PlayerVC {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    playerVC.pipController?.stopPictureInPicture()
+                }
+            }
+        }
+    }
+    
+    func topViewController(base: UIViewController?) -> UIViewController? {
+        if let nav = base as? UINavigationController {
+            return topViewController(base: nav.visibleViewController)
+        }
+        if let tab = base as? UITabBarController {
+            if let selected = tab.selectedViewController {
+                return topViewController(base: selected)
+            }
+        }
+        if let presented = base?.presentedViewController {
+            return topViewController(base: presented)
+        }
+        return base
+    }
+}
