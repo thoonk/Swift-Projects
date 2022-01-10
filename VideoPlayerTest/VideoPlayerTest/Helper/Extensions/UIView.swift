3d494802ae7bb80aebd98a9737bbe92b702829dc
thoonk <dkrmrm878@naver.com>
2022-01-10 16:28:27 +0900

    [Fix] clean up the codes.

---
+++ VideoPlayerTest/VideoPlayerTest/Helper/Extensions/UIView.swift
@@ -0,0 +1,36 @@
+//
+//  UIView.swift
+//  VideoPlayerTest
+//
+//  Created by thoonk on 2022/01/06.
+//
+
+import UIKit
+
+extension UIView {
+    var parentViewController: UIViewController? {
+        // Starts from next (As we know self is not a UIViewController).
+        var parentResponder: UIResponder? = self.next
+        while parentResponder != nil {
+            if let viewController = parentResponder as? UIViewController {
+                return viewController
+            }
+            parentResponder = parentResponder?.next
+        }
+        return nil
+    }
+    
+    func roundCorners(_ corners: UIRectCorner, radius: CGFloat) {
+        if #available(iOS 11.0, *) {
+            clipsToBounds = true
+            layer.cornerRadius = radius
+            layer.maskedCorners = CACornerMask(rawValue: corners.rawValue)
+        } else {
+            let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners,
+                                    cornerRadii: CGSize(width: radius, height: radius))
+            let mask = CAShapeLayer()
+            mask.path = path.cgPath
+            layer.mask = mask
+        }
+    }
+}
