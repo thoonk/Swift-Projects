dbb34e9ab80bb196538d772969dc8cadbbc634fe
thoonk <dkrmrm878@gmail.com>
2022-07-13 17:08:40 +0900

    [Add] RxSwiftPractice - Buffuer, Window, Debounce and Throttle code example

--- RxSwiftPractice/TimeBasedOperators.playground/Contents.swift
+++ RxSwiftPractice/TimeBasedOperators.playground/Contents.swift
@@ -6,46 +6,46 @@ import PlaygroundSupport
 
 let bag = DisposeBag()
 
-print("------replay------")
-let greetings = PublishSubject<String>()
-let replayGreetings = greetings.replay(2)
-replayGreetings.connect()
-
-greetings.onNext("1. hello")
-greetings.onNext("2. hi")
-greetings.onNext("3. welcome")
-
-replayGreetings
-    .subscribe(onNext: {
-        print($0)
-    })
-    .disposed(by: bag)
-
-// 구독 시점 이후에 발생한 이벤트이기 때문에 버퍼와 상관없이 방출됨
-greetings.onNext("4. What's up?")
-
-print("------replayAll------")
-let doctorStrange = PublishSubject<String>()
-let timeStone = doctorStrange.replayAll()
-timeStone.connect()
-
-doctorStrange.onNext("Dormammu")
-doctorStrange.onNext("I've come to bargain.")
-doctorStrange.onNext("Dormammu")
-doctorStrange.onNext("I've come to bargain.")
-
-timeStone
-    .subscribe(onNext: {
-        print($0)
-    })
-    .disposed(by: bag)
+//print("------replay------")
+//let greetings = PublishSubject<String>()
+//let replayGreetings = greetings.replay(2)
+//replayGreetings.connect()
+//
+//greetings.onNext("1. hello")
+//greetings.onNext("2. hi")
+//greetings.onNext("3. welcome")
+//
+//replayGreetings
+//    .subscribe(onNext: {
+//        print($0)
+//    })
+//    .disposed(by: bag)
+//
+//// 구독 시점 이후에 발생한 이벤트이기 때문에 버퍼와 상관없이 방출됨
+//greetings.onNext("4. What's up?")
+//
+//print("------replayAll------")
+//let doctorStrange = PublishSubject<String>()
+//let timeStone = doctorStrange.replayAll()
+//timeStone.connect()
+//
+//doctorStrange.onNext("Dormammu")
+//doctorStrange.onNext("I've come to bargain.")
+//doctorStrange.onNext("Dormammu")
+//doctorStrange.onNext("I've come to bargain.")
+//
+//timeStone
+//    .subscribe(onNext: {
+//        print($0)
+//    })
+//    .disposed(by: bag)
 
 print("------buffer------")
 // buffer는 array를 방출
 //let source = PublishSubject<String>()
 //var count = 0
 //let timer = DispatchSource.makeTimerSource()
-//
+
 //timer.schedule(deadline: .now() + 2, repeating: .seconds(1))
 //timer.setEventHandler {
 //    count += 1
@@ -53,13 +53,23 @@ print("------buffer------")
 //}
 //
 //timer.resume()
-//
+
+//Observable<Int>
+//    .interval(.seconds(1), scheduler: MainScheduler.instance)
+//    .buffer(timeSpan: .seconds(6), count: 2, scheduler: MainScheduler.instance)
+//    .take(3)
+//    .subscribe(onNext: {
+//        print($0)
+//    })
+//    .disposed(by: bag)
+
 //source
 //    .buffer(
 //        timeSpan: .seconds(2),
 //        count: 2,
 //        scheduler: MainScheduler.instance
 //    )
+//    .take(5)
 //    .subscribe(onNext: {
 //        print($0)
 //    })
@@ -67,11 +77,8 @@ print("------buffer------")
 
 print("------window------")
 // window는 observable을 방출
-//let maxObservableCnt = 3
-//let makeTime = RxTimeInterval.seconds(2)
-//
 //let window = PublishSubject<String>()
-//
+
 //var windowCnt = 0
 //let windowTimerSource = DispatchSource.makeTimerSource()
 //windowTimerSource.schedule(deadline: .now() + 2, repeating: .seconds(1))
@@ -80,18 +87,22 @@ print("------window------")
 //    window.onNext("\(windowCnt)")
 //}
 //windowTimerSource.resume()
-//
-//window
+
+//Observable<Int>
+//    .interval(.seconds(1), scheduler: MainScheduler.instance)
 //    .window(
-//        timeSpan: makeTime,
-//        count: maxObservableCnt,
+//        timeSpan: .seconds(2),
+//        count: 2,
 //        scheduler: MainScheduler.instance
 //    )
-//    .flatMap { windowObservable -> Observable<(index: Int, element: String)> in
-//        windowObservable.enumerated()
-//    }
+//    .take(5)
 //    .subscribe(onNext: {
-//        print("\($0.index)번째 Observable 요소 \($0.element)")
+//        $0.subscribe(onNext: { event in
+//            print(event)
+//        }, onCompleted: {
+//            print("end subdividing")
+//        })
+//        .disposed(by: bag)
 //    })
 //    .disposed(by: bag)
 
@@ -117,27 +128,28 @@ print("------delaySubscription------")
 
 print("------delay------")
 // 시퀀스 자체를 뒤로 미룸
-//let delaySubject = PublishSubject<Int>()
-//
-//var delayCnt = 0
-//let delayTimerSource = DispatchSource.makeTimerSource()
-//delayTimerSource.schedule(deadline: .now(), repeating: .seconds(1))
-//delayTimerSource.setEventHandler {
-//    delayCnt += 1
-//    delaySubject.onNext(delayCnt)
-//}
-//delayTimerSource.resume()
-//
-//delaySubject
-//    .delay(.seconds(3), scheduler: MainScheduler.instance)
-//    .subscribe(onNext: {
-//        print($0)
-//    })
-//    .disposed(by: bag)
+let delaySubject = PublishSubject<Int>()
+
+var delayCnt = 0
+let delayTimerSource = DispatchSource.makeTimerSource()
+delayTimerSource.schedule(deadline: .now(), repeating: .seconds(1))
+delayTimerSource.setEventHandler {
+    delayCnt += 1
+    delaySubject.onNext(delayCnt)
+}
+delayTimerSource.resume()
+
+delaySubject
+    .delay(.seconds(3), scheduler: MainScheduler.instance)
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: bag)
 
 print("------interval------")
 //Observable<Int>
 //    .interval(.seconds(3), scheduler: MainScheduler.instance)
+//    .take(3)
 //    .subscribe(onNext: {
 //        print($0)
 //    })
@@ -146,10 +158,11 @@ print("------interval------")
 print("------timer------")
 //Observable<Int>
 //    .timer(
-//        .seconds(5),
-//        period: .seconds(2),
+//        .seconds(1),
+//        period: .seconds(1),
 //        scheduler: MainScheduler.instance
 //    )
+//    .take(5)
 //    .subscribe(onNext: {
 //        print($0)
 //    })
@@ -157,18 +170,52 @@ print("------timer------")
 
 print("------timeout------")
 // 설정된 시간을 초과하면 에러 방출
-let button = UIButton(type: .system)
-button.setTitle("눌러주세요", for: .normal)
-button.sizeToFit()
+//let button = UIButton(type: .system)
+//button.setTitle("눌러주세요", for: .normal)
+//button.sizeToFit()
+//
+//PlaygroundPage.current.liveView = button
+//
+//button.rx.tap
+//    .do(onNext: {
+//        print("tapped")
+//    })
+//    .timeout(.seconds(5), scheduler: MainScheduler.instance)
+//    .subscribe(onNext: {
+//        print($0)
+//    })
+//    .disposed(by: bag)
 
-PlaygroundPage.current.liveView = button
+print("------debounce------")
+//let button = UIButton(type: .system)
+//button.setTitle("Debounce", for: .normal)
+//button.sizeToFit()
+//
+//var count = 0
+//
+//button.rx.tap
+//    .debounce(.seconds(3), scheduler: MainScheduler.instance)
+//    .subscribe(onNext: { _ in
+//        count += 1
+//        print(count)
+//    })
+//    .disposed(by: bag)
+//
+//PlaygroundPage.current.liveView = button
 
-button.rx.tap
-    .do(onNext: {
-        print("tapped")
-    })
-    .timeout(.seconds(5), scheduler: MainScheduler.instance)
-    .subscribe(onNext: {
-        print($0)
-    })
-    .disposed(by: bag)
+print("------throttle------")
+//let button = UIButton(type: .system)
+//button.setTitle("Throttle", for: .normal)
+//button.sizeToFit()
+//
+//var count = 0
+//
+//button.rx.tap
+//    .throttle(.seconds(3), scheduler: MainScheduler.instance)
+//    .subscribe(onNext: { _ in
+//        count += 1
+//        print(count)
+//    })
+//    .disposed(by: bag)
+//
+//PlaygroundPage.current.liveView = button
